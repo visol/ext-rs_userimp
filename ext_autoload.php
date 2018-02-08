90cc1c25f72cc93fed52c0ada677c3c96a281cf5
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2018-02-08 20:32:05 +0000

    [TASK] Require phpspreadsheet, remove ext_autoload.php

--- ext_autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('rs_userimp');
-return [
-    'PHPExcel' => $extensionPath . 'Resources/PHP/PHPExcel-1.8.1/Classes/PHPExcel.php',
-    'PHPExcel_IOFactory' => $extensionPath . 'Resources/PHP/PHPExcel-1.8.1/Classes/PHPExcel/IOFactory.php',
-];
